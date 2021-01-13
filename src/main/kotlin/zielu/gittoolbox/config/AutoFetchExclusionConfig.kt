027ce9f316350b08f84b33117df832985ef70200
Lukasz Zielinski <zielu@users.noreply.github.com>
2021-01-13 13:12:40 +0100

    Update toolchain and code layout

---
+++ src/main/kotlin/zielu/gittoolbox/config/AutoFetchExclusionConfig.kt
@@ -0,0 +1,37 @@
+package zielu.gittoolbox.config
+
+import com.intellij.util.xmlb.annotations.Transient
+
+internal data class AutoFetchExclusionConfig(
+  var repositoryRootPath: String = "",
+  var excludedRemotes: MutableList<RemoteConfig> = ArrayList()
+) {
+
+  constructor(repositoryRootPath: String) : this(repositoryRootPath, ArrayList())
+
+  @Transient
+  fun copy(): AutoFetchExclusionConfig {
+    return AutoFetchExclusionConfig(
+      repositoryRootPath,
+      ArrayList(excludedRemotes.map { it.copy() })
+    )
+  }
+
+  fun isRemoteExcluded(remoteName: String): Boolean {
+    return excludedRemotes.any { remoteName == it.name }
+  }
+
+  @Transient
+  fun noRemotes(): Boolean {
+    return excludedRemotes.isEmpty()
+  }
+
+  @Transient
+  fun hasRemotes(): Boolean {
+    return excludedRemotes.isNotEmpty()
+  }
+
+  fun removeRemote(remote: RemoteConfig): Boolean {
+    return excludedRemotes.remove(remote)
+  }
+}
---
+++ src/test/kotlin/zielu/gittoolbox/ui/DatePresenterTest.kt
@@ -0,0 +1,60 @@
+package zielu.gittoolbox.ui
+
+import com.intellij.openapi.util.SystemInfo
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.extension.ExtendWith
+import zielu.gittoolbox.config.AbsoluteDateTimeStyle
+import zielu.gittoolbox.config.DateType
+import java.time.Clock
+import java.time.ZonedDateTime
+
+@ExtendWith(MockKExtension::class)
+internal class DatePresenterTest {
+  @MockK
+  private lateinit var mockGateway: DatePresenterLocalGateway
+  private lateinit var datePresenter: DatePresenter
+
+  @BeforeEach
+  fun beforeEach() {
+    datePresenter = DatePresenterImpl(mockGateway)
+    every { mockGateway.getClock() } returns Clock.systemDefaultZone()
+    every { mockGateway.getAbsoluteDateTimeFormat() } returns AbsoluteDateTimeStyle.ISO_8601.format
+  }
+
+  @Test
+  fun `format should return expected value if hidden`() {
+    // when
+    val formatted = datePresenter.format(DateType.HIDDEN, ZonedDateTime.now())
+
+    // then
+    assertThat(formatted).isEmpty()
+  }
+
+  @TestFactory
+  fun `format should return not empty value`(): List<DynamicNode> {
+    val types = DateType.values().toMutableList()
+    types.remove(DateType.HIDDEN)
+    if (SystemInfo.isMac && SystemInfo.isOsVersionAtLeast("10.16")) {
+      // relative causes error on Big Sur
+      types.remove(DateType.RELATIVE)
+    }
+
+    return types.map { type ->
+      DynamicTest.dynamicTest("should return not empty value for $type") {
+        // when
+        val formatted = datePresenter.format(type, ZonedDateTime.now())
+
+        // then
+        assertThat(formatted).isNotBlank()
+      }
+    }
+  }
+}
